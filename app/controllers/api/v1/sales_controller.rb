b9198b9b683b298443ba49de54115b620b0dbc36
Leighton James <leighton.james@comms365.com>
2015-02-22 17:47:14 +0000

    Sales controller methods and tests

--- app/controllers/api/v1/sales_controller.rb
+++ app/controllers/api/v1/sales_controller.rb
@@ -2,6 +2,29 @@ class Api::V1::SalesController < ApplicationController
   before_action :authorize
   before_action :set_sale, only: [:show, :destroy]
   
+  def create
+    @sales = []
+    errors = []
+    params[:sales] = [params[:sale]] if params[:sale]
+
+    params[:sales].each do |s|
+      sale = Sale.new(s.permit(:sale_ts, :code, :value))
+      @sales.push sale
+      errors.push sale.errors if !sale.save
+    end
+
+    if errors.empty?
+      render :create, status: :created
+    else
+      render json: errors, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    @sale.destroy
+    head :no_content
+  end
+  
   private
   
   def set_sale
